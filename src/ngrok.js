717c4d84fc9f380f3c3e411272500a6972ae1660
ben40d9 <benpaley6@gmail.com>
2022-12-22 17:20:18 -0800

    took url function out

--- src/ngrok.js
+++ src/ngrok.js
@@ -1,17 +1,17 @@
-import ngrok from "ngrok";
+// import ngrok from "ngrok";
 
-//import dotenv
-import * as dotenv from "dotenv";
-dotenv.config();
+// //import dotenv
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
-//make var for our token so it is hidden
-const NGROK_TOKEN = process.env.NGROK_TOKEN;
+// //make var for our token so it is hidden
+// const NGROK_TOKEN = process.env.NGROK_TOKEN;
 
-//connect to ngrok server and return the tunnel they make
-export const url = await ngrok.connect({
-  proto: "http", // http|tcp|tls, defaults to http
-  addr: 8180, // port or network address, defaults to 80
-  authtoken: `${NGROK_TOKEN}`, // your authtoken from ngrok.com
-});
+// //connect to ngrok server and return the tunnel they make
+// export const url = await ngrok.connect({
+//   proto: "http", // http|tcp|tls, defaults to http
+//   addr: 8180, // port or network address, defaults to 80
+//   authtoken: `${NGROK_TOKEN}`, // your authtoken from ngrok.com
+// });
 
 // console.log(url);
