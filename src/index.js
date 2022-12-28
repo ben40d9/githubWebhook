5cd7b8d9d986e966fc9173a99f01bbae77ee9a11
ben40d9 <benpaley6@gmail.com>
2022-12-28 14:42:30 -0800

    Need to set the url to which the payloads will be delivered === the generated ngrok tunnel url

--- src/index.js
+++ src/index.js
@@ -24,28 +24,35 @@ import { gitHubActions, octokit } from "./octokit.js";
   });
   console.log(`This is our ngrok tunnel: ${url}`);
 
+  //get our specific webhook
   const specificHook = await gitHubActions.getRepoWebhook;
-  console.log(specificHook);
+  //the url to which the payloads will be delivered
+  console.log(specificHook.data.config.url);
 
-  await console.log(
-    `The amount of webhooks for this repo is : ${gitHubActions.listRepoWebhooks.data.length}`
-  );
+  let urlToPayloadDelivery = await specificHook.data.config.url;
+  urlToPayloadDelivery === `${url}`;
+  await console.log(urlToPayloadDelivery);
+  await console.log(specificHook.data.config.url);
 
-  //this will trigger the hook with the latest push to the current repository(has to be sub to push events)
-  const testPush = gitHubActions.testPushToRepoWebhook;
-  console.log(`The test push status is : ${testPush.status}`); //Status: 204 => No Content
+  // await console.log(
+  //   `The amount of webhooks for this repo is : ${gitHubActions.listRepoWebhooks.data.length}`
+  // );
 
-  //returns a list of webhook deliveries for a webhook configured in a repository
-  const listOfDelivered = await gitHubActions.listDeleveriesForARepo;
-  // console.log(listOfDelivered);
-  //the # of deliveries
-  console.log(
-    `The amount of webhook deliveries for THIS webhook in this repo is : ${listOfDelivered.data.length}`
-  );
+  // //this will trigger the hook with the latest push to the current repository(has to be sub to push events)
+  // const testPush = gitHubActions.testPushToRepoWebhook;
+  // console.log(`The test push status is : ${testPush.status}`); //Status: 204 => No Content
 
-  //this will trigger a ping event to be sent to the hook
-  const ping = await gitHubActions.pingRepoWebhook;
-  console.log(`ping: ${ping}`);
+  // //returns a list of webhook deliveries for a webhook configured in a repository
+  // const listOfDelivered = await gitHubActions.listDeleveriesForARepo;
+  // // console.log(listOfDelivered);
+  // //the # of deliveries
+  // console.log(
+  //   `The amount of webhook deliveries for THIS webhook in this repo is : ${listOfDelivered.data.length}`
+  // );
+
+  // //this will trigger a ping event to be sent to the hook
+  // const ping = await gitHubActions.pingRepoWebhook;
+  // console.log(`ping: ${ping}`);
 
   //
   app.listen(8180, () => {
