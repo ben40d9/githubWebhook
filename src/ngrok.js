6312f19db8bc7dff3f1a13728c4495b000a83828
ben40d9 <benpaley6@gmail.com>
2022-12-29 20:10:53 -0800

    export ngrok connect now

--- src/ngrok.js
+++ src/ngrok.js
@@ -1,17 +1,15 @@
-// import ngrok from "ngrok";
+import ngrok from "ngrok";
 
-// //import dotenv
-// import * as dotenv from "dotenv";
-// dotenv.config();
+//import dotenv
+import * as dotenv from "dotenv";
+dotenv.config();
 
-// //make var for our token so it is hidden
-// const NGROK_TOKEN = process.env.NGROK_TOKEN;
+//make var for our token so it is hidden
+import { NGROK_TOKEN } from "./hidden/index.js";
 
-// //connect to ngrok server and return the tunnel they make
-// export const url = await ngrok.connect({
-//   proto: "http", // http|tcp|tls, defaults to http
-//   addr: 8180, // port or network address, defaults to 80
-//   authtoken: `${NGROK_TOKEN}`, // your authtoken from ngrok.com
-// });
-
-// console.log(url);
+//connect to ngrok server and return the tunnel they make
+export const url = await ngrok.connect({
+  proto: "http", // http|tcp|tls, defaults to http
+  addr: 8180, // port or network address, defaults to 80
+  authtoken: `${NGROK_TOKEN}`, // your authtoken from ngrok.com
+});
