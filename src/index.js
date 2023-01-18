941f9cde226df0aa3434ccff0e803c913ae2bdab
ben40d9 <benpaley6@gmail.com>
2023-01-17 16:15:45 -0800

    brought combine.js file into here, just was making new "index" in a seperate file for dev purposes

--- src/index.js
+++ src/index.js
@@ -3,40 +3,44 @@ import ngrok from "ngrok";
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 import { url } from "./ngrok.js";
 
-// import { NGROK_TOKEN } from "./hidden/index.js";
-
-// //import dotenv
-// import * as dotenv from "dotenv";
-// dotenv.config();
-
-// //make var for our token so it is hidden
-// const NGROK_TOKEN = process.env.NGROK_TOKEN;
-
-import { gitHubActions, octokit } from "./octokit.js";
+import { gitHubActions, octokit, updateGhWebhookUrl } from "./octokit.js";
 
 (async () => {
+  await app.post("/webhook", async (req, res) => {
+    res.send(console.log(`this is our req body${req.body.repository}`));
+  });
+
+  await app.listen(8180, () => {
+    console.log("listening for product updates...");
+  });
+
   //connect to ngrok server and return the tunnel they make
-  const currentUrl = await url;
-  console.log(`This is our ngrok tunnel : ${currentUrl}`);
+  const tunnelUrl = await url;
+  console.log(`This is our ngrok tunnel : ${tunnelUrl}`);
 
-  // //get our specific webhook
-  // const specificHook = await gitHubActions.getRepoWebhook;
+  //get our specific webhook
+  const specificHook = await gitHubActions.getRepoWebhook;
 
-  // //the hooks data
-  // console.log(specificHook.data);
+  //the hooks data
+  console.log(specificHook.data);
 
-  // //the OLD url to which the payloads will be delivered (to show change)
-  // console.log(`the OLD url : ${specificHook.data.config.url}`);
+  //the OLD url to which the payloads will be delivered (to show change)
+  console.log(`the OLD url : ${specificHook.data.config.url}`);
+
+  //get the webhooks config
+  const webhookConfig = await gitHubActions.getWebhookConfig;
+  console.log(webhookConfig);
 
   // const updateWebhookConfig = await octokit.request(
   //   "PATCH /repos/{owner}/{repo}/hooks/{hook_id}/config",
   //   {
-  //     owner: "ben40d9",
-  //     repo: "next-blog",
-  //     hook_id: "393663340",
+  //     owner: "**",
+  //     repo: "**",
+  //     hook_id: "**",
   //     content_type: "json",
   //     //change here
   //     url: `${currentUrl}`,
@@ -45,41 +49,42 @@ import { gitHubActions, octokit } from "./octokit.js";
 
   // console.log(updateWebhookConfig);
 
-  // //change the url that the payloads will be delivered to equal our ngrok tunnel
-  // specificHook.data.config.url = await url;
+  //change the url that the payloads will be delivered to equal our ngrok tunnel
+  specificHook.data.config.url = await tunnelUrl;
 
-  // //url should be changed now
-  // await console.log(
-  //   `url should be changed now : ${specificHook.data.config.url}`
-  // );
+  //url should be changed now
+  await console.log(
+    `url should be changed now : ${specificHook.data.config.url}`
+  );
 
-  // // console.log(specificHook.data);
+  // console.log(specificHook.data);
 
-  // //amount of webhooks for this repo
-  // await console.log(
-  //   `The amount of webhooks for this repo is : ${gitHubActions.listRepoWebhooks.data.length}`
-  // );
+  //amount of webhooks for this repo
+  await console.log(
+    `The amount of webhooks for this repo is : ${gitHubActions.listRepoWebhooks.data.length}`
+  );
 
-  // //this will trigger the hook with the latest push to the current repository(has to be sub to push events)
-  // const testPush = gitHubActions.testPushToRepoWebhook;
-  // console.log(`The test push status is : ${testPush.status}`); //Status: 204 => No Content
+  //this will trigger the hook with the latest push to the current repository(has to be sub to push events)
+  const testPush = gitHubActions.testPushToRepoWebhook;
+  console.log(`The test push status is : ${testPush.status}`); //Status: 204 => No Content
 
-  // console.log(testPush);
+  console.log(testPush);
 
-  // //returns a list of webhook deliveries for a webhook configured in a repository
-  // const listOfDelivered = await gitHubActions.listDeleveriesForARepo;
-  // // console.log(listOfDelivered);
-  // //the # of deliveries
-  // console.log(
-  //   `The amount of webhook deliveries for THIS webhook in this repo is : ${listOfDelivered.data.length}`
-  // );
-  //test
+  //returns a list of webhook deliveries for a webhook configured in a repository
+  const listOfDelivered = await gitHubActions.listDeleveriesForARepo;
+  // console.log(listOfDelivered);
+  //the # of deliveries
+  console.log(
+    `The amount of webhook deliveries for THIS webhook in this repo is : ${listOfDelivered.data.length}`
+  );
 
-  // //this will trigger a ping event to be sent to the hook
-  // const ping = await gitHubActions.pingRepoWebhook;
-  // console.log(`ping : ${ping}`);
+  //this will trigger a ping event to be sent to the hook
+  const ping = await gitHubActions.pingRepoWebhook;
+  console.log(`ping : ${ping}`);
 
-  app.listen(8180, () => {
-    console.log("listening...");
-  });
+  // await updateGhWebhookUrl(tunnelUrl);
+
+  // console.log(`the NEW url : ${specificHook.data.config.url}`);
+
+  // console.log(`this is the updated config url: ${updatedWebhookConfig.url}`);
 })();
